c38a38cbd445efa8bcd13fbcedd2475ca50d2356
noirgif <nomaru@outlook.com>
2018-05-29 22:56:44 +0800

    debug sender module

--- sender.py
+++ sender.py
@@ -31,7 +31,7 @@ class EmailSender(Sender):
         from email.message import EmailMessage
         try:
             with SMTP(self.host) as smtp:
-                smtp.login(self.username, self.password)
+                smtp.login(self.address, self.password)
                 for recipient in self.recipients:
                     msg = EmailMessage()
                     msg.set_content(content)
@@ -39,5 +39,16 @@ class EmailSender(Sender):
                     msg['From'] = self.address
                     msg['To'] = recipient
                     smtp.send_message(msg)
-        except SMTPException:
-            return
+        except SMTPException as err:
+            print("SMTP Error: {}".format(err))
+
+if __name__ == '__main__':
+    import json
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+    email_account = config['email_account']
+    email_password = config['email_password']
+    recipients = config['recipients']
+    email_sender = EmailSender(email_account, email_password, recipients)
+    email_sender.send("""Email module test""",
+    """Testing if the email module functions normally""")
