949a3b270de0298b38f486ca768465506332a623
noirgif <nomaru@outlook.com>
2018-05-31 01:25:21 +0800

    rework error in GUI

--- gui_main.py
+++ gui_main.py
@@ -1,4 +1,5 @@
 import tkinter
+import tkinter.messagebox
 import json
 from spider import search
 from sender import ConsoleSender, EmailSender
@@ -105,13 +106,13 @@ def main():
             "email_password" : "my-mail-password"
             # "recipients" : ["first-recipient@example.com", "second-recipient@example.com"]
         }
-        print("""Configuration file not found
+        tkinter.messagebox.showerror("Error", """Configuration file not found
 please edit config.json file""")
         with open("config.json", "w") as config_file:
             json.dump(config, config_file, indent=4)
         exit(0)
     """If it is OK, run the event loop to scrape the websites and send messages"""
-    eventloop()
+    eventloop(email_sender)
 
 
 frame5 = tkinter.Frame(top)
